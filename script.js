269bbf4aeb36e9c67fff25c0c8689cce593374f4
festowl916 <misstryy1@gmail.com>
2026-01-04 23:58:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
      MODE TEST (PRO)
      Tukar true → false bila live
   ===================== */
-  const TEST_MODE = false;
+  const TEST_MODE = true;
   const TEST_NOW  = new Date("2026-04-15T12:00:00").getTime();
 
   const nowTime = () => TEST_MODE ? TEST_NOW : Date.now();
@@ -76,4 +76,5 @@ document.addEventListener("DOMContentLoaded", () => {
   setInterval(updateCountdown, 1000);
 
 });
+
 
