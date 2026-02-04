c727df0425baac32c719ae202d490fd33304a41a
festowl916 <misstryy1@gmail.com>
2026-02-05 02:15:27 +0800

    Update daftar.js

--- daftar.js
+++ daftar.js
@@ -1,34 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("daftarForm");
-  if (!form) return;
+const SCRIPT_URL = "PASTE_WEB_APP_URL_KAU";
 
-  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw6dDf5cmVAMNrJL1xdGslzvYCtI4y1LK_e6Y2s8AABb3nxHeNvIcz0LlVwPokKY-lx/exec";
+document.getElementById("daftarForm").addEventListener("submit", function(e){
+  e.preventDefault();
 
-  form.addEventListener("submit", function(e){
-    e.preventDefault();
+  const form = e.target;
+  const file = form.resit.files[0];
+  if (!file) return alert("Pilih fail dulu");
 
-    const file = form.resit.files[0];
-    if (!file) return alert("Upload resit dulu");
+  const reader = new FileReader();
+  reader.onload = function () {
 
-    const reader = new FileReader();
-    reader.onload = function () {
-      const data = {
-        nama_penuh: form.nama_penuh.value,
-        fileName: file.name,
-        fileType: file.type,
-        fileData: reader.result.split(",")[1]
-      };
-
-      fetch(SCRIPT_URL, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: { "Content-Type": "application/json" }
-      })
-      .then(()=>alert("Berjaya"))
-      .catch(()=>alert("Gagal"));
+    const data = {
+      nama_penuh: form.nama_penuh.value,
+      fileName: file.name,
+      fileType: file.type,
+      fileData: reader.result.split(",")[1]
     };
-    reader.readAsDataURL(file);
-  });
+
+    fetch(SCRIPT_URL, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    })
+    .then(()=>alert("Berjaya upload"))
+    .catch(()=>alert("Gagal"));
+  };
+
+  reader.readAsDataURL(file);
 });
-
-
