4f13f3eae97edc3e5d8324477b810d76ac0717f4
festowl916 <misstryy1@gmail.com>
2026-02-06 02:27:26 +0800

    Update daftar.js

--- daftar.js
+++ daftar.js
@@ -1,1 +1,37 @@
 
+
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw-Irmyl9MK242dz7nz8NQCJ4XWIv6GsCsUEL4-rnWEvF0VFVFMEwnxRY91JrEmG-ErBg/exec";
+
+document.getElementById("daftarForm").addEventListener("submit", function(e){
+  e.preventDefault();
+
+  const form = e.target;
+  const file = form.resit.files[0];
+  if (!file) return alert("Pilih fail dulu");
+
+  const reader = new FileReader();
+  reader.onload = function () {
+
+    const data = {
+      nama_penuh: form.nama_penuh.value,
+      fileName: file.name,
+      fileType: file.type,
+      fileData: reader.result.split(",")[1]
+    };
+
+    fetch(SCRIPT_URL, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    })
+    .then(res => res.text())
+    .then(text => {
+      alert(text);   // akan tunjuk BERJAYA atau ERROR
+    })
+    .catch(err => {
+      alert("Gagal: " + err);
+    });
+  };
+
+  reader.readAsDataURL(file);
+});
