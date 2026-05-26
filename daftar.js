5a23fdebbe8d7cf960f322cf3cf5ab82da1b3fae
festowl916 <misstryy1@gmail.com>
2026-05-27 01:52:32 +0800

    Update daftar.js

--- daftar.js
+++ daftar.js
@@ -170,13 +170,26 @@ const descBaju = document.getElementById("descBaju");
   if (!semakKategoriUmur()) return;
     /* =========================
    VALIDASI BAJU (WAJIB)
-   ========================= */
+      ========================= */
 if (form.jenis.value === "daftar+baju") {
 
   const saizRadio = form.querySelector("input[name='saiz_baju']:checked");
 
   if (!saizRadio) {
     alert("Sila pilih saiz baju.");
+    return;
+  }
+
+  const cara = document.getElementById("caraPenghantaran").value;
+  const alamat = document.getElementById("alamatField").value.trim();
+
+  if (!cara) {
+    alert("Sila pilih cara penghantaran.");
+    return;
+  }
+
+  if (cara === "pos" && !alamat) {
+    alert("Sila isi alamat penghantaran.");
     return;
   }
 
@@ -278,7 +291,9 @@ data.push({
   jenis: form.jenis.value,
   saiz_baju: saiz,
   catatan_baju: catatanGabung,
-  alamat: "WALK-IN",
+  alamat: document.getElementById("alamatField")
+  ? document.getElementById("alamatField").value
+  : "",
   fileName,
   fileType,
   fileData
